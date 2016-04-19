32a3ffe65713f96679de7a80dfdbc92b6beb7cc7
王义川 <2978439075@qq.com>
2016-04-19 10:55:16 +0800

    增加carthage支持

---
+++ FDFullscreenPopGesture/FDFullscreenPopGesture.h
@@ -0,0 +1,18 @@
+//
+//  FDFullscreenPopGesture.h
+//  FDFullscreenPopGesture
+//
+//  Created by 王义川 on 16/4/19.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FDFullscreenPopGesture.
+FOUNDATION_EXPORT double FDFullscreenPopGestureVersionNumber;
+
+//! Project version string for FDFullscreenPopGesture.
+FOUNDATION_EXPORT const unsigned char FDFullscreenPopGestureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDFullscreenPopGesture/PublicHeader.h>
+#import <FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.h>
